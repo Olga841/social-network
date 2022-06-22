57289e396ad628024251a9785f4d72364033c744
Olga Alekseeva <olgaalekseeva966@gmail.com>
2022-06-22 06:10:48 +0300

    Add styles to Sidebar.tsx fixed some styles to Post.tsx

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,15 +1,20 @@
 import React from "react";
 import s from './componrnts_styles/nav.module.css'
 import {NavLink} from "react-router-dom";
+import {Sidebar} from "./Sidebar";
 
 export function Nav() {
     return (
-     <nav className={s.nav}>
-        <div><NavLink to="/profiles">Profiles</NavLink></div>
-        <div><NavLink to="/messages">Messages</NavLink></div>
-        <div><NavLink to="/news">News</NavLink></div>
-        <div><NavLink to="/music">Music</NavLink></div>
-        <div><NavLink to="/settings">Settings</NavLink></div>
-      </nav>
+        <div className={s.nav}>
+            <nav>
+                <div><NavLink to="/profiles">Profiles</NavLink></div>
+                <div><NavLink to="/messages">Messages</NavLink></div>
+                <div><NavLink to="/news">News</NavLink></div>
+                <div><NavLink to="/music">Music</NavLink></div>
+                <div><NavLink to="/settings">Settings</NavLink></div>
+            </nav>
+            <Sidebar/>
+        </div>
+
     )
 }
--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import './componrnts_styles/item.css';
 
 
-
 export type PostPropsType = {
     post: string
     like: string
@@ -11,7 +10,7 @@ export type PostPropsType = {
 export function Post(props: PostPropsType) {
     return (
         <div className='post_item'>
-            <img  src="https://scontent-waw1-1.xx.fbcdn.net/v/t1.18169-9/23526_339558886310_1955560_n.jpg?_nc_cat=103&ccb=1-5&_nc_sid=9267fe&_nc_ohc=2hhoxzfy_TUAX8UDzmC&_nc_ht=scontent-waw1-1.xx&oh=00_AT-FwYJcZJBIdpcJ9-XrPXGZf3d91LJk0sDrKpXnRE9mLg&oe=62507450" alt="ava" />
+            <img src="https://klike.net/uploads/posts/2019-03/1551511801_1.jpg" alt="ava"/>
             <span>{props.post}</span>
             <br/>
             <span>{props.like}</span>
