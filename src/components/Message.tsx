c027fab7735481327308b8b851ebdb0653f47ce2
Olga Alekseeva <olgaalekseeva966@gmail.com>
2022-06-22 21:40:09 +0300

    Refactor styles in messages

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -1,10 +1,15 @@
 import s from "./componrnts_styles/Dialogs.module.css";
 import React from "react";
+import {FriendInfo} from "./FriendInfo";
 
 export type MessagePropsType = {
     message: string
 }
 
- export const Message = (props: MessagePropsType) => {
-    return (<div className={s.message}>{props.message}</div>    )
+export const Message = (props: MessagePropsType) => {
+    return (
+        <div className={s.message}>
+            <FriendInfo/>
+            <div>{props.message}</div>
+        </div>)
 }
