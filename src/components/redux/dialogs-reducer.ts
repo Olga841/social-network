4d61dbbbba4370253ccd4272eb2bdaea93564034
Olga Alekseeva <olgaalekseeva966@gmail.com>
2022-08-16 12:57:45 +0300

    refactor dialogs-reducer.ts (add types)

--- src/components/redux/dialogs-reducer.ts
+++ src/components/redux/dialogs-reducer.ts
@@ -1,13 +1,63 @@
-import {ActionsTypes, DialogsPageType, MessageType, PostType} from "./store";
+const ADD_MESSAGE = 'ADD-MESSAGE'
+const CHANGE_MESSAGE_TEXT = 'CHANGE-MESSAGE-TEXT'
 
-export function dialogsReducer(state: DialogsPageType, action: ActionsTypes) {
-    if (action.type === 'ADD-MESSAGE') {
+export type DialogType = {
+    id: number
+    name: string
+}
+export type MessageType = {
+    message: string
+}
+export type DialogsPageType = {
+    dialogs: Array<DialogType>
+    messages: Array<MessageType>
+    newMessage: string
+}
+type AddMessage = {
+    type: 'ADD-MESSAGE'
+    newMessage: string
+}
+type ChangeMessageText = {
+    type: 'CHANGE-MESSAGE-TEXT'
+    text: string
+}
+type ActionsTypes = AddMessage | ChangeMessageText
+
+const initialState = {
+    dialogs: [
+        {id: 1, name: 'Vasya'},
+        {id: 2, name: 'Valera'},
+        {id: 3, name: 'Vera'},
+        {id: 4, name: 'Vyacheslav'},
+        {id: 5, name: 'Vova'},
+        {id: 6, name: 'Vitya'}
+    ],
+    messages: [
+        {message: 'Hello!'},
+        {message: 'Hello!'},
+        {message: 'Hello!'},
+        {message: 'Hello!'},
+        {message: 'Hello!'},
+        {message: 'Hello!'},
+        {message: 'Hello!'},
+        {message: 'Hello!'},
+        {message: 'Hello!'}
+    ],
+    newMessage: 'Hello, my Dear friend!'
+}
+
+
+export function dialogsReducer(state: DialogsPageType = initialState, action: ActionsTypes): DialogsPageType {
+    if (action.type === ADD_MESSAGE) {
         let message: MessageType = {message: action.newMessage}
         state.messages.push(message)
         state.newMessage = ''
-    } else if (action.type === 'CHANGE-MESSAGE-TEXT') {
+    } else if (action.type === CHANGE_MESSAGE_TEXT) {
         state.newMessage = action.text
         console.log(state.newMessage)
     }
     return state;
 }
+
+export const addMessageAC = (newMessage: string) => ({type: ADD_MESSAGE, newMessage} as const)
+export const changeMessageTextAC = (text: string) => ({type: CHANGE_MESSAGE_TEXT, text} as const)
