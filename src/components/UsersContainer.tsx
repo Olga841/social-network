d822a2e7484a56373cdc00ee56a70a696d9f0137
Olga Alekseeva <olgaalekseeva966@gmail.com>
2022-09-27 19:33:31 +0300

    refactor UsersContainer.tsx

--- src/components/UsersContainer.tsx
+++ src/components/UsersContainer.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {connect} from "react-redux";
-import {followAC, UsersPageType, setUsersAC, unfollowAC, UserType} from "./redux/users-reducer";
+import {followAC, UsersPageType, setUsersAC, unfollowAC, UserType, setCurrentPageAC} from "./redux/users-reducer";
 import {AppStateType} from "./redux/redux-store";
 import {Dispatch} from "redux";
 import UsersC from "./UsersС";
@@ -9,19 +9,26 @@ import {Users} from "./Users";
 
 export type  MapStatePropsType = {
     users: Array<UserType>
+    pageSize: number
+    totalUsersCount: number
+    currentPage: number
 }
 
 const mapStateToProps = (state: AppStateType): MapStatePropsType => {
     return {
-        users: state.usersPage.users
+        users: state.usersPage.users,
+        pageSize: state.usersPage.pageSize,
+        totalUsersCount: state.usersPage.totalUsersCount,
+        currentPage: state.usersPage.currentPage
     }
 }
 const mapDispatchToProps = (dispatch: Dispatch) => {
     return {
         follow: (userID: number) => (dispatch(followAC(userID))),
         unfollow: (userID: number) => (dispatch(unfollowAC(userID))),
-        setUsers: (users: Array<UserType>) => (dispatch(setUsersAC(users)))
+        setUsers: (users: Array<UserType>, totalCount: number) => (dispatch(setUsersAC(users, totalCount))),
+        setCurrentPage: (currentPage: number) => (dispatch(setCurrentPageAC(currentPage)))
     }
 }
 
-export const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(Users)
+export const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(UsersC)
