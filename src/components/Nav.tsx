fcaa19177b81be7da5081006b0cef83484feda7a
Olga Alekseeva <olgaalekseeva966@gmail.com>
2022-10-09 22:18:37 +0300

    Add NavLink to UserProfile

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -7,8 +7,8 @@ export function Nav() {
     return (
         <div className={s.nav}>
             <nav>
-                <div><NavLink to="/profiles">Profiles</NavLink></div>
-                <div><NavLink to="/messages">Messages</NavLink></div>
+                <div><NavLink to="/profile">Profiles</NavLink></div>
+                <div><NavLink to={"/messages"}>Messages</NavLink></div>
                 <div><NavLink to="/news">News</NavLink></div>
                 <div><NavLink to="/music">Music</NavLink></div>
                 <div><NavLink to="/settings">Settings</NavLink></div>
