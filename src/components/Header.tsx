538aee86d31279810f404cddf515f55552bfe914
Olga Alekseeva <olgaalekseeva966@gmail.com>
2022-11-16 20:35:34 +0300

    refactor styles & import of styles in Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,19 @@
 import React from "react";
-import s from './componrnts_styles/header.module.css'
+import s from './components_styles/header.module.css'
 import {NavLink} from "react-router-dom";
 import {AuthPropsType} from "./HeaderContainer";
 
 export function Header(props: AuthPropsType) {
+    console.log(props)
     return (
         <header className={s.header}>
             <img src="https://cdn.logo.com/hotlink-ok/logo-social.png" alt="logo"/>
             <div className={s.loginBlock}>
-                <NavLink to={'/auth'}>Log In</NavLink>
+                {props.data.id ? <img
+                        src={'https://social-network.samuraijs.com/activecontent/images/users/2/user-small.jpg?v=0'}
+                        alt={'me'}/> :
+                    <NavLink to={'/login'}>Log In</NavLink>}
+
             </div>
         </header>
     )
