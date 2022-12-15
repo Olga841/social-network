b65ea0c280c3cd9380f9fa6f395c2f09a9368f2e
Olga Alekseeva <olgaalekseeva966@gmail.com>
2022-12-15 11:56:53 +0300

    add DataAccessLayer (file api.ts)

---
+++ src/api/api.ts
@@ -0,0 +1,30 @@
+import React from "react";
+import axios, {AxiosResponse} from "axios";
+
+
+export const usersAPI = {
+    getAllUsers(currentPage: number, pageSize: number) {
+        return axios.get<any>(`https://social-network.samuraijs.com/api/1.0/users?page=${currentPage}&count=${pageSize}`, {
+            withCredentials: true
+        }).then(response => {
+            return response
+        })
+    },
+    getUsersForPage(page: number, pageSize: number) {
+        return axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${page}&count=${pageSize}`, {
+            withCredentials: true
+        }).then(response => {
+            return response
+        })
+    },
+    followUnfollowUser(userId: number) {
+        return axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${userId}`, {}, {
+            withCredentials: true,
+            headers: {
+                'API-KEY': 'cf66b916-fda5-47ac-9f68-3fafeff56e6d'
+            }
+        }).then(response => {
+            return response
+        })
+    }
+}
