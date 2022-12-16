e987afb2db3a14fb8c0c5dfee2e7b84a3c9a406d
Olga Alekseeva <olgaalekseeva966@gmail.com>
2022-12-16 10:54:44 +0300

    change imports & refactor props

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import s from './components_styles/header.module.css'
+import s from '../components_styles/header.module.css'
 import {NavLink} from "react-router-dom";
 import {AuthPropsType} from "./HeaderContainer";
 
@@ -9,7 +9,7 @@ export function Header(props: AuthPropsType) {
         <header className={s.header}>
             <img src="https://cdn.logo.com/hotlink-ok/logo-social.png" alt="logo"/>
             <div className={s.loginBlock}>
-                {props.data.id ? <img
+                {props.response.data.id ? <img
                         src={'https://social-network.samuraijs.com/activecontent/images/users/2/user-small.jpg?v=0'}
                         alt={'me'}/> :
                     <NavLink to={'/login'}>Log In</NavLink>}
