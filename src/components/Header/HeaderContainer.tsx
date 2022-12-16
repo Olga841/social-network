9f6fc7a6a796ca645cc55da90e92b572e9f4ac00
Olga Alekseeva <olgaalekseeva966@gmail.com>
2022-12-16 10:55:09 +0300

    change imports & refactor props

--- src/components/Header/HeaderContainer.tsx
+++ src/components/Header/HeaderContainer.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import axios from "axios";
 import {Header} from "./Header";
-import {AuthType, ResponseDataType, setAuthData, toggleIsFetching} from "./redux/auth-reducer";
+import {AuthType, ResponseDataType, setAuthData, toggleIsFetching} from "../redux/auth-reducer";
 import {connect} from "react-redux";
-import {AppStateType} from "./redux/redux-store";
+import {AppStateType} from "../redux/redux-store";
 
 export type AuthPropsType = {
-    data: ResponseDataType
+    response: ResponseDataType
     isFetching: boolean
     setAuthData: (data: ResponseDataType) => void
     toggleIsFetching: (isFetching: boolean) => void
@@ -30,11 +30,7 @@ class HeaderContainer extends React.Component <AuthPropsType> {
 
 const mapStateToProps = (state: AppStateType): AuthType => {
     return {
-        data: {
-            id: state.auth.data.id,
-            email: state.auth.data.email,
-            login: state.auth.data.login
-        },
+        response: state.auth.response,
         isFetching: state.auth.isFetching
     }
 }
