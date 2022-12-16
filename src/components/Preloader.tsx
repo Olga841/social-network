95795b9b8825f17e6aa2e2563a16bafe4aa51199
Olga Alekseeva <olgaalekseeva966@gmail.com>
2022-12-16 10:59:12 +0300

    refactor imports

--- src/components/Preloader.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import preloader from "../images/Heart-1s-200px.gif";
-
-
-export const Preloader = () => {
-    return (<div>
-        <img src={preloader}/>
-    </div>)
-}
