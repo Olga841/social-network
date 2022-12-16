988b3f826a38ba0d3703cc22341b2f4c7c6b5021
Olga Alekseeva <olgaalekseeva966@gmail.com>
2022-12-16 13:48:10 +0300

    refactor auth & Header props

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,22 @@
 import React from "react";
 import s from '../components_styles/header.module.css'
 import {NavLink} from "react-router-dom";
-import {AuthPropsType} from "./HeaderContainer";
 
-export function Header(props: AuthPropsType) {
+export type HeaderPropsType = {
+    data: {
+        id: number | null
+        email: string | null
+        login: string | null
+    }
+}
+
+export function Header(props: HeaderPropsType) {
     console.log(props)
     return (
         <header className={s.header}>
             <img src="https://cdn.logo.com/hotlink-ok/logo-social.png" alt="logo"/>
             <div className={s.loginBlock}>
-                {props.response.data.id ? <img
+                {props.data.id ? <img
                         src={'https://social-network.samuraijs.com/activecontent/images/users/2/user-small.jpg?v=0'}
                         alt={'me'}/> :
                     <NavLink to={'/login'}>Log In</NavLink>}
